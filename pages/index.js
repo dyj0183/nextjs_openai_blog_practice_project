2fe77625efca8067da3fffa1c079efe1b1b839e2
Yu-Chun Tung <dyj0183@byui.edu>
2023-07-24 23:06:47 -0600

    set up auth0 to work on main page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div />;
+  return (<div><h1>Home page</h1>
+  <div><Link href="/api/auth/login">LOGIN</Link></div></div>);
 }
